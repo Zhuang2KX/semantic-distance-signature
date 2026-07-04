d1b28904577f46fd1d6beefe392090030dd04083
Kaixiang Zhuang <oldzhuang@outlook.com>
2026-07-04 12:27:45 +0800

    Add files via upload

---
+++ Code/mvpa/function/apply_model_tpls.m
@@ -0,0 +1,19 @@
+function out_apply = apply_model_tpls(xtest,ytest,out_train,temporary_file)
+
+betamap = out_train.feature_weight;
+bias = out_train.bias;
+
+out_apply.dp = bias + xtest*betamap;  
+
+out_apply.tv = ytest;
+out_apply.feature_weight = betamap;
+out_apply.bias = bias;
+out_apply.isclassification = isfield(out_train,'isclassification') && out_train.isclassification;
+
+if out_apply.isclassification
+    class_labels = out_train.class_labels;
+    out_apply.pv = class_labels(1) * ones(size(out_apply.dp));
+    out_apply.pv(out_apply.dp > out_train.class_threshold) = class_labels(2);
+else
+    out_apply.pv = out_apply.dp;
+end
